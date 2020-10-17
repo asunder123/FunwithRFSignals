3795f308bbf7c1f3abfa0f2d4ce54f705941eda9
asunder123 <50335583+asunder123@users.noreply.github.com>
2020-10-17 20:56:47 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,13 +5,11 @@
 #    -v /dev/ttyACM0:/dev/ttyACM0 \
 #    -v /dev/ttyUSB0:/dev/ttyUSB0 \
 docker run \
-    --rm \
     --network=host \
     --privileged \
-    -v /tmp/./.X11-DISPLAY:0/tmp/.X11-unix \
     -v /dev:/dev \
     -v $HOME/topics:/topics \
     -v $HOME/topics/arduino:/home/developer/Arduino \
     --name arduino \
     tombenke/darduino:latest \
-    arduino
+    arduino \
